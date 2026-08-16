e6902f71950f30f549174c87696998697e0c5f64
Prakhar <prakharp677@gmail.com>
2026-08-16 14:59:07 +0530

    Updated JS file with deployed backend link

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const API_BASE = "http://127.0.0.1:2200";
+  const API_BASE = "https://wellness-iq.onrender.com";
 
   const form = document.getElementById("predict-form");
   const submitBtn = document.getElementById("submit-btn");
